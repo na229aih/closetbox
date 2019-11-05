c8d7b7bf0cc6eb754d1bd826e1d9f4a28a11857e
na229aih <naoya.aihara.1127@gmail.com>
2019-11-05 17:02:46 +0900

    items_controller.rb追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(
+      image: item_params[:image],
+      name: item_params[:name],
+      category: item_params[:category],
+      size: item_params[:size],
+      brand: item_params[:brand],
+      user_id: current_user.id
+    )
+    redirect_to "/users/#{current_user.id}"
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :category, :size, :brand)
+  end
+
 end
