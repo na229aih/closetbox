83d8ec4ab967ec180d488c6cac6bdb45b28fde5e
na229aih <naoya.aihara.1127@gmail.com>
2019-11-06 19:56:39 +0900

    routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root 'items#index'
+
+  root 'coordinates#index'
 
   resources :users ,only: [:show]
+
   resources :items ,only: [:new, :create]
+
+  resources :coordinates ,only: [:new, :create] do
+    collection do
+      get '/select_outer' => 'coordinates#select_outer', defaults: { format: 'json' }
+      get '/select_tops' => 'coordinates#select_tops', defaults: { format: 'json' }
+      get '/select_pants' => 'coordinates#select_pants', defaults: { format: 'json' }
+      get '/select_shoes' => 'coordinates#select_shoes', defaults: { format: 'json' }
+      get '/select_bag' => 'coordinates#select_bag', defaults: { format: 'json' }
+      get '/select_accessory' => 'coordinates#select_accessory', defaults: { format: 'json' }
+    end
+  end
+
 end
