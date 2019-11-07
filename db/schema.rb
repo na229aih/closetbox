5f66dfc89c412de900c18dc39618caee43952441
na229aih <naoya.aihara.1127@gmail.com>
2019-11-07 19:32:20 +0900

    migrate comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_123954) do
+ActiveRecord::Schema.define(version: 2019_11_07_074458) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "coordinate_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["coordinate_id"], name: "index_comments_on_coordinate_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "coordinate_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id"
@@ -55,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_11_05_123954) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "coordinates"
+  add_foreign_key "comments", "users"
   add_foreign_key "coordinate_items", "coordinates"
   add_foreign_key "coordinate_items", "items"
   add_foreign_key "coordinates", "users"
