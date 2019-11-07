b6fb113a295b53515ac21159d160d622cf7bdb65
na229aih <naoya.aihara.1127@gmail.com>
2019-11-07 12:30:08 +0900

    users_controller.rb修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @outer_items = Item.where(user_id: params[:id], category: "アウター")
-    @tops_items = Item.where(user_id: params[:id], category: "トップス")
-    @pants_items = Item.where(user_id: params[:id], category: "パンツ")
-    @shoes_items = Item.where(user_id: params[:id], category: "靴")
-    @bag_items = Item.where(user_id: params[:id], category: "バッグ")
-    @accessory_items = Item.where(user_id: params[:id], category: "小物")
+    @outer_items = Item.where(user_id: params[:id], category: "アウター").order("id DESC")
+    @tops_items = Item.where(user_id: params[:id], category: "トップス").order("id DESC")
+    @pants_items = Item.where(user_id: params[:id], category: "パンツ").order("id DESC")
+    @shoes_items = Item.where(user_id: params[:id], category: "靴").order("id DESC")
+    @bag_items = Item.where(user_id: params[:id], category: "バッグ").order("id DESC")
+    @accessory_items = Item.where(user_id: params[:id], category: "小物").order("id DESC")
   end
 
 end
