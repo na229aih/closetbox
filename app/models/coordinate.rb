bd2d4d7f3cb105ca5c7cb2b4dee8bc742a446014
na229aih <naoya.aihara.1127@gmail.com>
2019-11-09 14:15:24 +0900

    coordinate.rb修正

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,6 +1,6 @@
 class Coordinate < ApplicationRecord
   belongs_to :user
-  has_many :coordinate_items
+  has_many :coordinate_items, dependent: :destroy
   has_many :items, through: :coordinate_items
   has_many :comments, dependent: :destroy
 end
