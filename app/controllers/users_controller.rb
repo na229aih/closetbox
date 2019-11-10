4aed9824643f8f529c9cf5bd1470acafee9135e6
na229aih <naoya.aihara.1127@gmail.com>
2019-11-10 21:35:22 +0900

    users_controller.rb追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,18 @@ class UsersController < ApplicationController
     @accessory_items = Item.where(user_id: params[:id], category: "小物").order("id DESC")
   end
 
+  def follow
+    Relationship.create(
+      user_id: current_user.id,
+      follow_id: params[:follow_id]
+    )
+    @user = User.find(params[:follow_id])
+  end
+
+  def unfollow
+    following = current_user.relationships.find_by(follow_id: params[:follow_id])
+    following.destroy
+    @user = User.find(params[:follow_id])
+  end
+
 end
