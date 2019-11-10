263cea3eab265c2d258bcbe8e3804dbe3721adcb
na229aih <naoya.aihara.1127@gmail.com>
2019-11-10 21:35:42 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :coordinates
   has_many :comments
   has_many :likes, dependent: :destroy
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 end
